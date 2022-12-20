597ae9cac1f08b1e9c251ff1ce6417e69552dd31
jon-myers <jon.myers.sound@gmail.com>
2022-12-20 14:22:37 -0800

    added visual symbol

--- src/components/EditorAudioPlayer.vue
+++ src/components/EditorAudioPlayer.vue
@@ -570,8 +570,8 @@ export default {
           const when = Number(startTime) + Number(time) * Number(traj.durTot);
           const curVal = this.pluckNode.cutoff.value;
           this.pluckNode.cutoff.setValueAtTime(curVal, when);
-          this.pluckNode.cutoff.linearRampToValueAtTime(0, when + this.lagTime);
-          this.pluckNode.cutoff.linearRampToValueAtTime(curVal, when + 2 * this.lagTime);
+          this.pluckNode.cutoff.linearRampToValueAtTime(0, when + 2 * this.lagTime);
+          this.pluckNode.cutoff.linearRampToValueAtTime(curVal, when + 4 * this.lagTime);
         })
       }
     },
