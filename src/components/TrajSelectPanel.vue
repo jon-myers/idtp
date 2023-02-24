11ac6afaf6ab87412612e0ac62df92c9eb7f7fba
jon-myers <jon.myers.sound@gmail.com>
2023-02-24 13:22:02 -0500

    some more visual tweaks

--- src/components/TrajSelectPanel.vue
+++ src/components/TrajSelectPanel.vue
@@ -1,7 +1,7 @@
 <template>
 <div class='main'>
   <div class='selectionPanel'>
-    <div class='selectionRow'>
+    <div class='selectionRow checks'>
       <label>Pluck</label>
       <input 
         v-if='editable' 
@@ -16,6 +16,9 @@
         @change='updateBool'
         disabled='disabled'
       />
+    </div>
+    <div class='selectionRow checks'>
+      
       <label class='spaceLeft'>Dampen</label>
       <input 
         v-if='editable' 
@@ -30,7 +33,9 @@
         @change='updateDampen'
         disabled='disabled'
       />
-      <label v-if='showVibObj' class='spaceLeft'>Up</label>
+    </div>
+    <div class='selectionRow checks'>
+      <label v-if='showVibObj' class='spaceLeft'>Phase</label>
       <input
         v-if='editable && showVibObj'
         type='checkbox'
@@ -407,11 +412,12 @@ export default {
 
 .selectionPanel {
   width: v-bind(ctrlBoxWidth + 'px');
-  height: 140px;
+  height: 190px;
   border-top: 1px solid black;
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
+  align-items: center;
+  justify-content: top;
 }
 
 .imgContainer {
@@ -466,6 +472,17 @@ label {
   flex-direction: row;
   align-items: center;
   justify-content: left;
+}
+
+.selectionRow.checks {
+  justify-content: right;
+  margin-right: 20px;
+  width: 100%;
+  height: 30px;
+}
+
+.selectionRow > label {
+  margin-right: 5px;
 }
 
 .spaceLeft {
