102fba34f2c06988af4141ede9fe57d4f2a4db36
jon-myers <jon.myers.sound@gmail.com>
2023-07-15 10:30:45 -0700

    added vowel

--- src/js/query.ts
+++ src/js/query.ts
@@ -15,7 +15,8 @@ class Query {
     category = 'trajectoryID',
     pitch = undefined,
     sequenceLength = undefined,
-    trajectoryID = undefined
+    trajectoryID = undefined,
+    vowel = undefined
   }: {
     type?: (
     'phrase' | 
@@ -24,10 +25,11 @@ class Query {
     'connectedSequenceOfTrajectories'
     ),
     designator?: 'includes' | 'excludes' | 'startsWith' | 'endsWith',
-    category?: 'trajectoryID'| 'pitch',
+    category?: 'trajectoryID'| 'pitch' | 'vowel',
     pitch?: Pitch,
     sequenceLength?: number,
-    trajectoryID?: number
+    trajectoryID?: number,
+    vowel?: string
   } = {}) {
     this.identifier = [];
     this.instrumentIdx = 0;
@@ -175,6 +177,71 @@ class Query {
           }
         });
       }
+    } else if (category === 'vowel') {
+      const inst = piece.instrumentation[this.instrumentIdx];
+      if (!(inst === 'Vocal (F)' || inst === 'Vocal (M)')) {
+        throw new Error('category vowel is only for vocal instruments');
+      }
+      if (vowel === undefined) {
+        throw new Error('vowel is required when category is vowel');
+      }
+      if (type === 'phrase') {
+        const phrases = piece.phrases;
+        const filteredPhrases = phrases.filter(phrase => {
+          const trajs = phrase.trajectories;
+          const vowels = trajs.map(traj => traj.vowel);
+          return this.vowelDifferentiate(vowel, designator, vowels);
+        });
+        this.trajectories = filteredPhrases.map(phrase => phrase.trajectories);
+        this.identifier = filteredPhrases.map(phrase => phrase.pieceIdx!);
+      } else if (type === 'group') {
+        const groups = piece.allGroups({ instrumentIdx: this.instrumentIdx });
+        const filteredGroups = groups.filter(group => {
+          const trajs = group.trajectories;
+          const vowels = trajs.map(traj => traj.vowel);
+          const boolean = this.vowelDifferentiate(vowel, designator, vowels);
+          if (boolean) {
+            this.identifier.push(group.id);
+          }
+          return boolean;
+        });
+        this.trajectories = filteredGroups.map(group => group.trajectories);
+      } else if (type === 'sequenceOfTrajectories') {
+        if (sequenceLength === undefined) {
+          throw new Error('sequenceLength is required when type is ' + 
+            'sequenceOfTrajectories');
+        }
+        const allTrajs = piece.allTrajectories(this.instrumentIdx);
+        this.trajectories = [];
+        for (let i = 0; i < allTrajs.length - sequenceLength + 1; i++) {
+          const trajSeq = allTrajs.slice(i, i + sequenceLength);
+          const vowels = trajSeq.map(traj => traj.vowel);
+          const boolean = this.vowelDifferentiate(vowel, designator, vowels);
+          if (boolean) {
+            this.trajectories.push(trajSeq);
+            const idObj = {
+              phraseIdx: trajSeq[0].phraseIdx!,
+              trajIdx: trajSeq[0].num!
+            }
+            this.identifier.push(idObj);
+          }
+        }
+      } else if (type === 'connectedSequenceOfTrajectories') {
+        const allTrajs = piece.allTrajectories(this.instrumentIdx);
+        const subGroups = this.splitTrajsBySilences(allTrajs)
+        subGroups.forEach(trajs => {
+          const vowels = trajs.map(traj => traj.vowel);
+          const boolean = this.vowelDifferentiate(vowel, designator, vowels);
+          if (boolean) {
+            this.trajectories.push(trajs);
+            const idObj = {
+              phraseIdx: trajs[0].phraseIdx!,
+              trajIdx: trajs[0].num!
+            }
+            this.identifier.push(idObj);
+          }
+        });
+      }
     }
   }
 
@@ -217,6 +284,20 @@ class Query {
     return boolean;
   }
 
+  private vowelDifferentiate(vowel: string, designator: string, vowels: (string | undefined)[]) {
+    let boolean: boolean = false;
+    if (designator === 'includes') {
+      boolean = vowels.includes(vowel);
+    } else if (designator === 'excludes') {
+      boolean = !vowels.includes(vowel);
+    } else if (designator === 'startsWith') {
+      boolean = vowels[0] === vowel;
+    } else if (designator === 'endsWith') {
+      boolean = vowels[vowels.length - 1] === vowel;
+    }
+    return boolean;
+  }
+
   public static async create({
     transcriptionID = '63445d13dc8b9023a09747a6',
     type = 'phrase',
@@ -224,7 +305,8 @@ class Query {
     category = 'trajectoryID',
     pitch = undefined,
     sequenceLength = undefined,
-    trajectoryID = undefined
+    trajectoryID = undefined,
+    vowel = undefined,
   }: {
     type?: (
       'phrase' | 
@@ -234,10 +316,11 @@ class Query {
     ),
     transcriptionID?: string,
     designator?: 'includes' | 'excludes' | 'startsWith' | 'endsWith',
-    category?: 'trajectoryID'| 'pitch',
+    category?: 'trajectoryID'| 'pitch' | 'vowel',
     pitch?: Pitch,
     sequenceLength?: number,
-    trajectoryID?: number
+    trajectoryID?: number,
+    vowel?: string,
   } = {}) {
     try {
       const piece = await instantiatePiece(transcriptionID);
@@ -247,7 +330,8 @@ class Query {
         category, 
         pitch, 
         sequenceLength,
-        trajectoryID 
+        trajectoryID,
+        vowel
       };
       return new Query(piece, queryObj);
     } catch (error) {
@@ -256,12 +340,16 @@ class Query {
   }
 }
 
-const pitch = new Pitch({ swara: 'ga', oct: 0 });
+// const pitch = new Pitch({ swara: 'ga', oct: 0 });
+
+const transcriptionID = '645ff354deeaf2d1e33b3c44';// beghum akhtar - babul mora
 Query.create({ 
   type: 'phrase', 
-  category: 'trajectoryID', 
-  designator: 'includes',
-  trajectoryID: 13, 
+  category: 'vowel', 
+  designator: 'startsWith',
+  trajectoryID: 13,
+  transcriptionID,
+  vowel: 'ō'
 })
   .then(q => {
     if (q !== undefined) {
