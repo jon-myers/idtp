9dcbbdda342419247d3bd245726e1570d8df2155
Jon Myers <jon.myers.sound@gmail.com>
2024-02-18 10:28:38 -0800

    just getting going

---
+++ src/comps/editor/SarangiSynth.vue
@@ -0,0 +1,27 @@
+<template>
+</template>
+
+<script lang='ts'>
+
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  name: 'SarangiSynth',
+  data() {
+    return {
+      freq: 400
+    }
+  },
+  props: {
+    ac: {
+      type: Object as PropType<AudioContext>,
+      required: true
+    },
+  },
+  mounted() {
+    this.del1 = this.ac.createDelay(1);
+    this.del2 = this.ac.createDelay(1);
+
+  }
+})
+</script>
