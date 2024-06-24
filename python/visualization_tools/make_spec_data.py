77ffdb86a854447057ea4dd17e5420d619a67697
Jon Myers <jon.myers.sound@gmail.com>
2024-06-24 17:03:59 -0400

    color and offset selection

--- python/visualization_tools/make_spec_data.py
+++ python/visualization_tools/make_spec_data.py
@@ -22,18 +22,16 @@ class NumpyEncoder(json.JSONEncoder):
 
 
 
-file_id = sys.argv[1]
-sa = float(sys.argv[2])
-if len(sys.argv) > 3:
-    full_path = sys.argv[3]
+
+full_path = sys.argv[1]
 loader = ess.EasyLoader(filename = full_path, replayGain=0, startTime=0)
 audio = loader()
 octaves = 3
 offset = 0.1
 params = {
           'inputSize': audio.size,
-          'minFrequency': 50,
-          'maxFrequency': 800,
+          'minFrequency': 75,
+          'maxFrequency': 2400,
           'binsPerOctave': 72,
           'windowSizeFactor': 1,
           'gamma': 20
@@ -45,7 +43,7 @@ max_samples = int(max_seconds * loader.paramValue('sampleRate'))
 
 passes = math.ceil(len(audio) / max_samples)
 arrs = []
-print(file_id)
+
 for i in range(passes):
     print(i)
     if i < passes - 1:
@@ -59,13 +57,15 @@ for i in range(passes):
 
 arr = np.hstack(arrs)
 arr =  (256 * (arr - np.min(arr)) / (np.max(arr) - np.min(arr))).astype(np.uint8)
+# flip vertically
+arr = np.flipud(arr)
 shape = np.shape(arr)
 arr_bytes = arr.tobytes()
 
 compressed_data = gzip.compress(arr_bytes)
 # json_data = json.dumps({'data': arr.tolist()}, cls=NumpyEncoder)
-with open('python/visualization_tools/gray_dir/test.gz', 'wb') as f:
+with open('python/visualization_tools/gray_dir/tall_test.gz', 'wb') as f:
     f.write(compressed_data)
     
-with open('python/visualization_tools/gray_dir/test_shape.json', 'w') as f:
+with open('python/visualization_tools/gray_dir/tall_test_shape.json', 'w') as f:
     json.dump({'shape': shape}, f)
--- src/comps/TestSpectrogram.vue
+++ src/comps/TestSpectrogram.vue
@@ -4,13 +4,59 @@
     <div class='canvas-container'>
       <canvas ref='canvas'></canvas>
     </div>
+    <div class='controls'>
+      <div class='controls-row'>
+        <label>Sa Freq</label>
+        <input type='number' v-model='saFreq' @change='updateLims'>
+        <button @click='updateSa(saFreq)'>Update</button>
+      </div>
+      <div class='controls-title-row'>
+        <label>Limits as octave offsets from Sa</label>
+      </div>
+      <div class='controls-row'>
+        <label>High oct offset</label>
+        <input 
+          type='number' 
+          v-model='highOctOffset'
+          step='0.01'
+          :max='highOctMaxOffset'
+          min='0'>
+      </div>
+      <div class='controls-row'>
+        <label>Low oct offset</label>
+        <input 
+          type='number'
+          v-model.number='lowOctOffset' 
+          step='0.01'
+          :max='lowOctMaxOffset'
+          min='0'
+          >
+      </div>
+      <div class='controls-title-row'>
+        <label>Colormap</label>
+        <select 
+          v-model='selectedCMapString'
+          @change='updateSa(saFreq)'
+          >
+          <option 
+            v-for='(cMap, i) in cMaps'
+            :key='i'
+            :value='cMap'
+            >{{cMap}}</option>
+        </select>
+      </div>
+    </div>
   </div>
 </template>
 <script lang='ts'>
 import { defineComponent } from 'vue';
 import * as cMap from 'd3-scale-chromatic';
+
+const isSequentialScheme = (key: string) => {
+  return key.startsWith('interpolate');
+}
+const sequentialSchemes = Object.keys(cMap).filter(isSequentialScheme);
 import { rgb } from 'd3-color';
-// import img from '@/assets/gray.webp';
 import pako from 'pako';
 // import * as tf from '@tensorflow/tfjs';
 
@@ -19,7 +65,17 @@ type TestSpectrogramDataType = {
   imgData?: ImageData,
   ctx?: CanvasRenderingContext2D | null,
   data: Uint8Array[],
-  testLastTime: number
+  croppedData: Uint8Array[],
+  testLastTime: number,
+  saFreq: number,
+  extRange: [number, number],
+  logOffset: number,
+  shapeData: {shape: [number, number]},
+  croppedShapeData: { shape: [number, number] },
+  lowOctOffset: number,
+  highOctOffset: number,
+  cMaps: string[],
+  selectedCMapString: string
 }
 export default defineComponent({
   name: 'TestSpectrogram',
@@ -28,42 +84,92 @@ export default defineComponent({
       imgData: undefined,
       ctx: undefined,
       data: [],
-      testLastTime: 0
+      croppedData: [],
+      testLastTime: 0,
+      saFreq: 368,
+      extRange: [75, 2400],
+      logOffset: 0.1,
+      shapeData: {shape: [0, 0]},
+      croppedShapeData: { shape: [0, 0] },
+      lowOctOffset: 1.1,
+      highOctOffset: 2.1,
+      cMaps: sequentialSchemes,
+      selectedCMapString: 'interpolateViridis'
     }
   },
 
   async mounted() {
-    const res = await fetch('https://swara.studio/test/test.gz');
+    const res = await fetch('https://swara.studio/test/tall_test.gz');
     const buf = await res.arrayBuffer();
-    const shape = await fetch('https://swara.studio/test/test_shape.json');
-    const shapeData = await shape.json() as {shape: [number, number]};
+    const shape = await fetch('https://swara.studio/test/tall_test_shape.json');
+    this.shapeData = await shape.json() as {shape: [number, number]};
     const oneDData = pako.inflate(new Uint8Array(buf));
-
-    let max = 0
-    for (let i = 0; i < shapeData.shape[0]; i++) {
-      const slice = oneDData.slice(i * shapeData.shape[1], (i + 1) * shapeData.shape[1]);
-
+    for (let i = 0; i < this.shapeData.shape[0]; i++) {
+      const slice = oneDData.slice(i * this.shapeData.shape[1], (i + 1) * this.shapeData.shape[1]);
       this.data.push(slice);
-      max = Math.max(max, ...this.data[i]);
     }
-    console.log('max', max);
-    console.log(shapeData.shape)
-    this.setUpCanvas(shapeData.shape);
-    // this.updateCanvas(shapeData.shape)
+    const logSa = Math.log2(this.saFreq);
+    const low = logSa - this.lowOctOffset;
+    const high = logSa + this.highOctOffset;
+    this.setNewLims(low, high);
+    this.setUpCanvas(this.croppedShapeData.shape);
+    await this.updateAllCols(this.croppedShapeData.shape);
+  },
 
-    const updateAllCols = async (shape: [number, number]) => {
+  computed: {
+    
+    highOctMaxOffset() {
+      const full = Math.log2(this.extRange[1] / this.saFreq);
+      return Math.floor(full * 100) / 100;
+    },
+
+    lowOctMaxOffset() {
+      const full = Math.log2(this.saFreq / this.extRange[0]);
+      return Math.floor(full * 100) / 100;
+    }
+
+  },
+
+  methods: {
+
+    updateLims() {
+      if (this.highOctOffset > this.highOctMaxOffset) {
+        this.highOctOffset = this.highOctMaxOffset;
+      }
+      if (this.lowOctOffset > this.lowOctMaxOffset) {
+        this.lowOctOffset = this.lowOctMaxOffset;
+      }
+    },
+
+    async updateAllCols(shape: [number, number]) {
       const numCols = 50;
       for (let i = 0; i < Math.ceil(shape[1] / numCols); i++) {
         await this.updateColumns(shape, i * numCols, numCols);
       }
-    }
-    updateAllCols(shapeData.shape);
+    },
 
-    // this.drawOnCanvas(this.data, shapeData.shape);
-    
-  },
+    async updateSa(freq: number) {
+      this.saFreq = freq;
+      const logSa = Math.log2(this.saFreq);
+      const newLogMin = logSa - this.lowOctOffset;
+      const newLogMax = logSa + this.highOctOffset;
+      this.setNewLims(newLogMin, newLogMax);
+      await this.updateAllCols(this.croppedShapeData.shape);
+    },
 
-  methods: {
+    setNewLims(logMin: number, logMax: number) {
+      const oldLogMin = Math.log2(this.extRange[0]);
+      const oldLogMax = Math.log2(this.extRange[1]);
+      const oldHeight = this.shapeData.shape[0];
+      let yMin = (logMin - oldLogMin) / (oldLogMax - oldLogMin) * oldHeight;
+      let yMax = (logMax - oldLogMin) / (oldLogMax - oldLogMin) * oldHeight;
+      yMin = Math.round(yMin);
+      yMax = Math.round(yMax);
+      const newHeight = yMax - yMin;
+      this.croppedData = this.data.slice(oldHeight - yMax, oldHeight - yMin);
+      this.croppedShapeData.shape = [newHeight, this.shapeData.shape[1]];
+      this.setUpCanvas(this.croppedShapeData.shape);
+    },
 
     setUpCanvas(shape: [number, number]) {
       const canvas = this.$refs.canvas as HTMLCanvasElement;
@@ -74,79 +180,53 @@ export default defineComponent({
         throw new Error('Could not get canvas context');
       }
       this.imgData = this.ctx.createImageData(shape[1], shape[0]);
-      console.log('done setting up')
     },
 
-    
-    // updateCanvas(shape: [number, number]) {
-    //   if (!this.ctx) {
-    //     throw new Error('Could not get canvas context');
-    //   }
-    //   let idx = 0;
-    //   let j = 0; // Initialize column index
-
-    //   const updateColumn = () => {
-    //     for (let i = 0; i < shape[0]; i++) {
-    //       idx = (i * shape[1] + j) * 4;
-    //       const color = rgb(cMap.interpolateViridis(this.data[i][j] / 255));
-    //       this.imgData!.data[idx] = color.r;
-    //       this.imgData!.data[idx + 1] = color.g;
-    //       this.imgData!.data[idx + 2] = color.b;
-    //       this.imgData!.data[idx + 3] = 255;
-    //     }
-    //     this.ctx.putImageData(this.imgData!, 0, 0, j, 0, 1, shape[0]); // Update only the current column
-
-    //     j++; // Move to the next column
-    //     if (j < shape[1]) {
-    //       requestAnimationFrame(updateColumn); // Schedule next column update
-    //     }
-    //   };
-
-    //   updateColumn(); // Start updating columns
-    // }
-
-    updateCanvas(shape: [number, number]) {
+    async updateColumns(
+        shape: [number, number], 
+        startIdx: number, 
+        numColumns: number,
+        selectedCMap: (x: number) => string = cMap[this.selectedCMapString]
+      ){
       if (!this.ctx) {
         throw new Error('Could not get canvas context');
       }
-      let idx = 0;
-      for (let i = 0; i < shape[0]; i++) {
-        for (let j = 0; j < shape[1]; j++) {
-          idx = (i * shape[1] + j) * 4;
-          const color = rgb(cMap.interpolateViridis(this.data[i][j] / 255));
-          this.imgData!.data[idx] = color.r
-          this.imgData!.data[idx + 1] = color.g;
-          this.imgData!.data[idx + 2] = color.b;
-          this.imgData!.data[idx + 3] = 255;
-        }
-      }
-      this.ctx.putImageData(this.imgData!, 0, 0);
-    },
-
-      async updateColumns(shape: [number, number], startIdx: number, numColumns: number) {
-        if (!this.ctx) {
-          throw new Error('Could not get canvas context');
-        }
-        const update = () => { 
-          for (let x = startIdx; x < startIdx + numColumns && x < shape[1]; x++) {
-            for (let i = 0; i < shape[0]; i++) {
-              const idx = (i * shape[1] + x) * 4;
-              const color = rgb(cMap.interpolateViridis(this.data[i][x] / 255));
-              this.imgData!.data[idx] = color.r;
-              this.imgData!.data[idx + 1] = color.g;
-              this.imgData!.data[idx + 2] = color.b;
-              this.imgData!.data[idx + 3] = 255;
-            }
+      
+      
+      const update = (selectedCMap: (x: number) => string) => {
+        for (let x = startIdx; x < startIdx + numColumns && x < shape[1]; x++) {
+          for (let i = 0; i < shape[0]; i++) {
+            const idx = (i * shape[1] + x) * 4;
+            const color = selectedCMap(this.croppedData[i][x] / 255);
+            const colorObj = rgb(color);
+            
+            this.imgData!.data[idx] = colorObj.r;
+            this.imgData!.data[idx + 1] = colorObj.g;
+            this.imgData!.data[idx + 2] = colorObj.b;
+            this.imgData!.data[idx + 3] = 255;
           }
-          this.ctx!.putImageData(this.imgData!, 0, 0, startIdx, 0, numColumns, shape[0]);
-        };
-        return new Promise<void>(resolve => {
-          requestAnimationFrame(() => {
-            update();
-            resolve();
-          });
+        }
+
+        this.ctx!.putImageData(this.imgData!, 0, 0, startIdx, 0, numColumns, shape[0]);
+        // draw sa
+        const logSa = Math.log2(this.saFreq);
+        const logMin = logSa - this.lowOctOffset;
+        const logMax = logSa + this.highOctOffset;
+        const y = shape[0] - (logSa - logMin) / (logMax - logMin) * shape[0];
+
+        this.ctx!.strokeStyle = 'red';
+        this.ctx!.beginPath();
+        this.ctx!.moveTo(startIdx, y);
+        this.ctx!.lineTo(startIdx + numColumns, y);
+        this.ctx!.stroke();
+      };
+      return new Promise<void>(resolve => {
+        requestAnimationFrame(() => {
+          update(selectedCMap);
+          resolve();
         });
-      }
+      });
+    }
 
   }
 })
@@ -170,17 +250,38 @@ img {
   display: inline-block;
   overflow: scroll;
 }
-/* 
-.img-container::before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: red;
-  opacity: 0.3;
-  pointer-events: none;
-} */
 
+.controls {
+  display: flex;
+  flex-direction: column;
+}
+
+.controls-row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.controls-row > * {
+  margin-right: 5px;
+  margin-left: 5px;
+}
+
+
+.controls-row > input {
+  width: 45px;
+} 
+
+.controls-title-row {
+  margin-top: 10px;
+  margin-bottom: 5px;
+  font-size: 1.2em;
+  font-weight: bold;
+  text-align: left;
+}
+
+.controls-title-row > label {
+  margin-right: 5px;
+  margin-left: 5px;
+}
 </style>
