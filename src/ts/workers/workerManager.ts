148ea6c331f379adf28e5ee0a54aa6d54a206077
Jon Myers <jon.myers.sound@gmail.com>
2024-10-25 20:52:51 -0400

    fix mimetype issue

--- src/ts/workers/workerManager.ts
+++ src/ts/workers/workerManager.ts
@@ -1,5 +1,5 @@
 let worker: Worker | undefined;
-const workerURL = new URL('@/ts/workers/spectrogramWorker.ts', import.meta.url);
+const workerURL = new URL('@/ts/workers/spectrogramWorker.js', import.meta.url);
 export const getWorker = (): Worker => {
   if (!worker) {
     worker = new Worker(workerURL, { type: 'module' });
