79d566b12be66ed82a8efc095026964655c9b7f5
Jon Myers <jon.myers.sound@gmail.com>
2024-11-22 14:28:14 -0800

    cleaning up

--- python/auto_transcribe/segment.py
+++ python/auto_transcribe/segment.py
@@ -2,7 +2,9 @@ import essentia.standard as ess
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-
+import sys
+sys.path.append('articulation_classification')
+from articulation_classification import AudioClassifier
 import requests
 def get_melograph_json(rec_id: str):
     url = f"https://swara.studio/melographs/{rec_id}/melograph.json"
@@ -15,7 +17,6 @@ def get_melograph_json(rec_id: str):
         return None
 
 rec_id = "62fa903990b9ba8cdae9d251"
-# breakpoint()
 melograph_data = get_melograph_json(rec_id)
 data_chunks_list = melograph_data['data_chunks']
 time_chunks_starts = melograph_data['time_chunk_starts']
@@ -30,15 +31,3 @@ filtered_start_idxs = [i for i, start in enumerate(time_chunks_starts) if start 
 data_chunks_list = [data_chunks_list[i] for i in filtered_start_idxs]
 time_chunks_list = [time_chunks_list[i] for i in filtered_start_idxs]
 
-
-
-# plotting
-# plt.figure(figsize=(10, 6))
-# for time_chunk, data_chunk in zip(time_chunks_list, data_chunks_list):
-#     plt.semilogy(time_chunk, data_chunk)
-
-# plt.xlabel('Time (s)')
-# plt.ylabel('Frequency (Hz)')
-# plt.title('Pitch Chunks on Log-Scaled Plot')
-# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-# plt.show()
